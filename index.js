fa85b161d3f7bcd9aaa50eee6c20374156047138
Jitka Badhan Nebeska <jitkabadhan@gmail.com>
2021-04-29 20:48:41 +0200

    JS - shirt changes color after click on the buttons

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
 'use strict';
 
 console.log('funguju');
+
+const changeColor = (event) => {
+  const shirtImg = document.querySelector('#product-image');
+  const color = event.target.classList.value;
+
+  if (color === 'white') {
+    shirtImg.style = 'fill: white';
+  } else if (color == 'black') {
+    shirtImg.style = 'fill: black';
+    shirtImg.style.stroke = 'white';
+  } else if (color === 'red') {
+    shirtImg.style = 'fill: #9d3232';
+  } else if (color === 'blue') {
+    shirtImg.style = 'fill: skyblue';
+  } else {
+    console.log('Nastala chyba uvnitř funkce changeColor.');
+  }
+};
+
+document.querySelectorAll('.button > button').forEach((item) => {
+  item.addEventListener('click', changeColor);
+});

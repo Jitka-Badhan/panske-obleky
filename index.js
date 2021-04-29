53319ada6abb739f729632ccf791c14a91ccecfc
Jitka Badhan Nebeska <jitkabadhan@gmail.com>
2021-04-29 21:27:46 +0200

    JS - order button shows an order in console

--- index.js
+++ index.js
@@ -2,9 +2,11 @@
 
 console.log('funguju');
 
+let color = '';
+
 const changeColor = (event) => {
   const shirtImg = document.querySelector('#product-image');
-  const color = event.target.classList.value;
+  color = event.target.classList.value;
 
   if (color === 'white') {
     shirtImg.style = 'fill: white';
@@ -22,4 +24,9 @@ const changeColor = (event) => {
 
 document.querySelectorAll('.button-div > button').forEach((item) => {
   item.addEventListener('click', changeColor);
+});
+
+document.querySelector('.product__order').addEventListener('click', () => {
+  const timeNow = new Date();
+  console.log(`The customer orders a ${color} shirt on date: ${timeNow}.`);
 });
